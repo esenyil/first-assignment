80a56faaaa9dc41b7e1489e93b690bc0f9c6bf86
esenyil <esenyildirim88@gmail.com>
2022-02-03 15:20:16 +0100

    commit

--- assignment.js
+++ assignment.js
@@ -5,13 +5,61 @@ const transactions = require('./simple-transaction-data').data;
 // Assignment 1:
 const filterFunction = (data) => {
     return data.filter((transaction) => {
-        // TODO
+        if(transaction.credit_card_company == 'VISA'){
+            const date = new Date(transaction.transaction_date);
+            const year = date.getFullYear()
+            if( year >= 2015){
+                if(transaction.price > 50){
+                    return transaction
+                }
+                
+            }
+        }
+
+
+
     });
 }
-
+console.log(filterFunction(transactions))
 
 // Assignment 2:
 const aboveAverageFunction = (data) => {
+    let sum = 0;
+    data.forEach(item => {
+        sum = sum + parseFloat(item.price);
+    })
+    let average = sum / data.length;
+    console.log(sum)
+    console.log(average)
+    return data.map(transaction => {
+        if (parseFloat(transaction.price) < average) {
+            transaction.isAboveAverage = false;
+            return transaction;
+        } else {
+            transaction.isAboveAverage = true;
+            return transaction
+        }
+    })
 
 }
+
+console.log(aboveAverageFunction(transactions))
+
+// const aboveAverageFunction = (data) => {
+//     let sum = 0;
+//     for (let i = 0; i < data.length; i++) {
+//         sum = sum + parseFloat(data[i].price);
+//     }
+//     let average = sum / data.length;
+//     return data.map(transaction => {
+//         if (parseFloat(transaction.price) < average) {
+//             transaction.isAboveAverage = false;
+//             return transaction;
+//         } else {
+//             transaction.isAboveAverage = true;
+//             return transaction
+//         }
+//     })
+
+// }
 
